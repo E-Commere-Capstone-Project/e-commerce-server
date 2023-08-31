2efa348a7e1153c983c110c5bbf73d10c76a1843
fmagmych <mychaelmagnuson@gmail.com>
2023-08-31 17:14:01 -0500

    Making the get product by id request work

--- api/products.js
+++ api/products.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllProducts } = require("../db/products");
+const { getAllProducts, getProductById } = require("../db/products");
 
 // GET = /api/products - get all products
 router.get("/", async (req, res, next) => {
@@ -9,6 +9,17 @@ router.get("/", async (req, res, next) => {
     const products = await getAllProducts();
 
     res.send(products);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET = /api/products/:id - get a single product by id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const product = await getProductById(req.params.id);
+
+    res.send(product);
   } catch (error) {
     next(error);
   }
--- db/products.js
+++ db/products.js
@@ -10,7 +10,26 @@ async function getAllProducts() {
     throw error;
   }
 }
+// GET = /api/products/:id - get a single product by id
+async function getProductById(id) {
+  try {
+    const {
+      rows: [product],
+    } = await client.query(`SELECT * FROM product WHERE id = $1;`, [id]);
+
+    return product;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// CREATE/POST = /api/products - add a new product
+
+// UPDATE/PATCH = /api/products/:id update/patch an existing product
+
+// DELETE = /api/products/:id delete a product
 
 module.exports = {
   getAllProducts,
+  getProductById,
 };
