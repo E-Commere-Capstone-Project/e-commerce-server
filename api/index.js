d3b2aa32ce9f08bb0b3fba459d66768d054baab4
fmagmych <mychaelmagnuson@gmail.com>
2023-09-01 01:14:18 -0500

    Making sure api index has working routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// ROUTER: /api/users
+router.use("/users", require("./users"));
+
 // ROUTER: /api/products
 router.use("/products", require("./products"));
 
