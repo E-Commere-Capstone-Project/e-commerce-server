890dd71ade42eb05e9333c0d6288e4047c5c2107
fmagmych <mychaelmagnuson@gmail.com>
2023-09-01 01:13:47 -0500

    Creating the user file in api and db to be able to register and login with user data in order to create functionality with creating products as a user

---
+++ api/users.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const { createUser, getUserByUsername, getUser } = require("../db");
+// const { requireUser } = require("./utils");
+const { JWT_SECRET = "neverTell" } = process.env;
+
+// POST /api/users/register
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, password, first_name, last_name, telephone } = req.body;
+    const queriedUser = await getUserByUsername(username);
+    if (queriedUser) {
+      res.status(401);
+      next({
+        name: "UserExistsError",
+        message: "A user by that username already exists",
+      });
+    } else if (password.length < 8) {
+      res.status(401);
+      next({
+        name: "PasswordLengthError",
+        message: "Password Too Short!",
+      });
+    } else {
+      const user = await createUser({
+        username,
+        password,
+        first_name,
+        last_name,
+        telephone,
+      });
+      if (!user) {
+        next({
+          name: "UserCreationError",
+          message: "There was a problem registering you. Please try again.",
+        });
+      } else {
+        const token = jwt.sign(
+          { id: user.id, username: user.username },
+          JWT_SECRET,
+          { expiresIn: "1w" }
+        );
+        res.send({ user, message: "you're signed up!", token });
+      }
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST /api/users/login
+router.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and a password",
+    });
+  }
+
+  try {
+    const user = await getUser({ username, password });
+    if (!user) {
+      next({
+        name: "IncorrectCredentialsError",
+        message: "Username or password is incorrect",
+      });
+    } else {
+      const token = jwt.sign(
+        { id: user.id, username: user.username },
+        JWT_SECRET,
+        { expiresIn: "1w" }
+      );
+      res.send({
+        user,
+        message: "you're logged in!",
+        token,
+        user_role: user.role_name,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+module.exports = router;
--- db/users.js
+++ db/users.js
@@ -2,6 +2,7 @@ const client = require("./client.js");
 const bcrypt = require("bcrypt");
 const SALT_COUNT = 10;
 
+// CREATE/POST /api/users/register create new account
 async function createUser({
   username,
   password,
@@ -15,7 +16,7 @@ async function createUser({
       rows: [user],
     } = await client.query(
       `
-      INSERT INTO users(username, password, first_name, last_name, telephone) VALUES ($1, $2, $3, $4, $5)
+      INSERT INTO users (username, password, first_name, last_name, telephone) VALUES ($1, $2, $3, $4, $5)
       ON CONFLICT (username) DO NOTHING RETURNING id, username
       `,
       [username, hashedPassword, first_name, last_name, telephone]
@@ -26,6 +27,64 @@ async function createUser({
   }
 }
 
+async function getUserByUsername(userName) {
+  // first get the user
+  try {
+    const { rows } = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE username = $1;
+    `,
+      [userName]
+    );
+    // if it doesn't exist, return null
+    if (!rows || !rows.length) return null;
+    // if it does:
+    // delete the 'password' key from the returned object
+    const [user] = rows;
+    // delete user.password;
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// POST /api/users/login log in to an existing user account
+async function getUser({ username, password }) {
+  if (!username || !password) {
+    return;
+  }
+
+  try {
+    const user = await getUserByUsername(username);
+    if (!user) return;
+    const hashedPassword = user.password;
+    const passwordsMatch = await bcrypt.compare(password, hashedPassword);
+    if (!passwordsMatch) return;
+    delete user.password;
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+// GET /api/users/me view logged in user account
+async function getUserById(userId) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`SELECT * FROM users WHERE id = $1;`, [userId]);
+    if (!user) return null;
+    delete user.password;
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+// GET /api/users/:username/cart get the logged in user's cart
 module.exports = {
   createUser,
+  getUserByUsername,
+  getUser,
+  getUserById,
 };
