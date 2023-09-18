b1da2c272de920fab28ad8df735725f7b7ad9fb0
fmagmych <mychaelmagnuson@gmail.com>
2023-09-18 18:58:50 -0500

    Making sure the module exports has all the right routes

--- db/index.js
+++ db/index.js
@@ -3,4 +3,5 @@ module.exports = {
   ...require("./users"), // adds key/values from users.js
   ...require("./products"),
   ...require("./product_categories.js"), // etc
+  ...require("./cart"),
 };
