c65949e7250c7548f08f0782246ff5f7d5166cdc
fmagmych <mychaelmagnuson@gmail.com>
2023-09-18 18:52:31 -0500

    Adding the route for the cart to work

--- api/index.js
+++ api/index.js
@@ -7,4 +7,7 @@ router.use("/users", require("./users"));
 // ROUTER: /api/products
 router.use("/products", require("./products"));
 
+// ROUTER: /api/cart
+router.use("/cart", require("./cart"));
+
 module.exports = router;
