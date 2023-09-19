048a73b133b343649021b49ef474973f255ab500
fmagmych <mychaelmagnuson@gmail.com>
2023-09-18 19:03:50 -0500

    Building functions to work for CRUD principles

---
+++ db/cart.js
@@ -0,0 +1,102 @@
+const client = require("./client");
+
+// GET all items in the cart
+async function getCart(userId) {
+  try {
+    const { rows: order_items } = await client.query(
+      `SELECT * FROM order_items WHERE user_id = $1`,
+      [userId]
+    );
+    console.log(`Checking cart: `, await checkInCart(userId, 1));
+
+    return order_items;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function checkInCart(userId, productId) {
+  try {
+    const {
+      rows: [order_items],
+    } = await client.query(
+      `SELECT * FROM order_items WHERE user_id = $1 AND product_id = $2`,
+      [userId, productId]
+    );
+    return order_items;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// POST add items to the cart
+async function addToCart(userId, productId, quantity) {
+  try {
+    // select an item from the products and simulate adding it to the cart
+    // realistically this function would only happen on the products route
+    // get product by id, create a new order_items row by inserting the product id, the cart id taken from the order_details and passing a quantity
+    // console.log(`Checking cart: `, checkInCart(userId, productId));
+    const ifInCart = await checkInCart(userId, productId);
+
+    if (ifInCart) {
+      const {
+        rows: [order_items],
+      } = await client.query(
+        `UPDATE order_items SET quantity = $1 WHERE user_id = $2 AND product_id = $3;`,
+        [quantity, userId, productId]
+      );
+      return order_items;
+    } else {
+      const {
+        rows: [order_items],
+      } = await client.query(
+        `INSERT INTO order_items ("product_id", "user_id", "quantity") VALUES ($1, $2, $3) RETURNING *;`,
+        [productId, userId, quantity]
+      );
+      return order_items;
+    }
+
+    // return order_items;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function removeFromCart(userId, productId) {
+  try {
+    const ifInCart = await checkInCart(userId, productId);
+
+    if (ifInCart) {
+      const {
+        rows: [order_items],
+      } = await client.query(
+        `DELETE FROM order_items WHERE user_id = $1 AND product_id = $2 RETURNING *;`,
+        [userId, productId]
+      );
+      return order_items;
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function emptyCart(userId) {
+  try {
+    const {
+      rows: [order_items],
+    } = await client.query(
+      `DELETE FROM order_items WHERE user_id = $1 RETURNING *;`,
+      [userId]
+    );
+    return order_items;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  getCart,
+  addToCart,
+  removeFromCart,
+  emptyCart,
+};
