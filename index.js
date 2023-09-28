b1f2f5e8a8313978d05cf32996b485e2700d68a6
fmagmych <mychaelmagnuson@gmail.com>
2023-09-28 08:51:15 -0500

    Minor changes

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Header", "Content-Type");
 });
